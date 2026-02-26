030973457c6677b7f8aaf21f9903ec69787e1605
Amin Alves <amin.alves.jr@gmail.com>
2026-02-25 21:44:32 -0400

    caminho layout app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import Sidebar from "../../components/Sidebar"
+import Sidebar from "../components/Sidebar"
 
 export default function SistemaLayout({
   children,
